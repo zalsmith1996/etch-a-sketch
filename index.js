87c6ac441eeaa1928388882b4ab6acc2ac3b8864
Zack <zalsmith1996@gmail.com>
2024-01-15 15:40:27 -0500

    Add event listners

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const container = document.querySelector('.container');
-const divs = [];
+const erase = document.querySelector('.erase');
+let divs = [];
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        divs[i, j]= document.createElement('div');
-        container.append(divs[i, j]);
+createGrid();
+
+function createGrid() {
+    for (let i = 0; i < 16; i++) {
+        for (let j = 0; j < 16; j++) {
+            divs[i, j] = document.createElement('div');
+            container.append(divs[i, j]);
+            divs[i, j].addEventListener('mouseover', changeBackgroundColor);
+        }
     }
+
+}
+
+erase.addEventListener('click', resetBackground);
+
+function resetBackground() {
+    divs = [];
+    while (container.hasChildNodes()) {
+        container.removeChild(container.lastChild);
+    }
+    createGrid();
+}
+
+function changeBackgroundColor() {
+    this.style.backgroundColor = 'red';
 }
